139cebbac65a6c4987fa07909454655ed47b9179
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2019-12-01 16:39:37 -0500

    Smaller filesize

--- ex.py
+++ ex.py
@@ -1,27 +1,9 @@
-def ex2():
+def ex():
     print()
-    squaredNumber = int(input("Type the number to be squared: "))
-    print()
-    print(squaredNumber * squaredNumber)
-    print()
-def ex3():
-    print("Please note that this is the same as running {}")
-    print()
-    cubedNumber = int(input("Type the number to be cubed: "))
-    print()
-    print(cubedNumber * cubedNumber * cubedNumber)
-    print()
-def ex4():
-    print()
-    exponentedNumber = int(input("Type the number to be exponented: "))
-    print()
-    print(exponentedNumber * exponentedNumber * exponentedNumber * exponentedNumber)
-    print()
-def ex5():
-    print()
-    num2Ex = input("Number to exponent?")
-    print()
-    print(num2Ex * num2Ex * num2Ex * num2Ex * num2Ex)
+    exNum = int(input("Number to be exponented? "))
+    pwrNum = int(input("To the power of what? "))
+    print('''
+    ANSWER: ''', pwrNum ** exNum)
 def ex1():
 	print()
 	num2Ex = input("Number to be exponented? ")
@@ -29,6 +11,3 @@ def ex1():
 	print("Unbelievable!")
 	print("I hope you're joking — you're asking this math: " +num2Use)
 	print("There's not even any addition! The answer is " +num2Use)
-def ex6():
-	cubedNumber = int(input("Number to be exponented? "))
-	print((cubedNumber * cubedNumber * cubedNumber) + (cubedNumber * cubedNumber * cubedNumber))
